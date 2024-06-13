619ab8e89b95b70f79e66868c4aa07ae6c0c5dd3
Jordi <rauxasoft@gmail.com>
2024-06-13 10:26:10 +0200

    pruebas de cambios...

--- src/main/java/com/sinensia/superpollo/business/model/Categoria.java
+++ src/main/java/com/sinensia/superpollo/business/model/Categoria.java
@@ -3,6 +3,8 @@ package com.sinensia.superpollo.business.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+// Xavi si, Laporta no
+
 public class Categoria implements Serializable{
 
 	private Long id;
