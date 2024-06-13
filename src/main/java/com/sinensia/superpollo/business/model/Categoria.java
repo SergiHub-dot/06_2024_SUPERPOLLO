ac311efa73c421f53b774109875c716ba763cbe0
Jordi <rauxasoft@gmail.com>
2024-06-13 10:33:44 +0200

    Mensaje de Turno

--- src/main/java/com/sinensia/superpollo/business/model/Categoria.java
+++ src/main/java/com/sinensia/superpollo/business/model/Categoria.java
@@ -3,6 +3,8 @@ package com.sinensia.superpollo.business.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+// Jordi
+
 public class Categoria implements Serializable{
 
 	private Long id;
