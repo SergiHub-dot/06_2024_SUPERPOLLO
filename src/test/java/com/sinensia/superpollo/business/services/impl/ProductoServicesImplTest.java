8145b640dd38c952599e5c5661bfaecce2183c02
Jordi <rauxasoft@gmail.com>
2024-07-17 11:39:58 +0200

    fuera controlador de pruebas y warnings

--- src/test/java/com/sinensia/superpollo/business/services/impl/ProductoServicesImplTest.java
+++ src/test/java/com/sinensia/superpollo/business/services/impl/ProductoServicesImplTest.java
@@ -3,7 +3,6 @@ package com.sinensia.superpollo.business.services.impl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
